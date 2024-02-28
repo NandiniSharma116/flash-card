a9738272a497e0d4061a0add9fa2a8f3f8869c7e
NandiniSharma116 <nandinisharma1603@gmail.com>
2024-02-29 00:50:25 +0530

    Connected with ts

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,44 @@
-import './style.css'
+import './style.css';
 
-console.log("Hello World");
+const langText = <HTMLSpanElement>document.querySelector("#langText");
+const Card = <HTMLDivElement>document.querySelector("#Card");
+
+interface CardItem {
+  text: string;
+  translation: string;
+}
+
+const words: CardItem[] = [
+  {
+    text: "Кот",
+    translation: "Cat",
+  },
+  {
+    text: "кровать",
+    translation: "Bed",
+  },
+  {
+    text: "Привет",
+    translation: "Hello",
+  },
+  {
+    text: "Врач",
+    translation: "Doctor",
+  },
+];
+
+const showWord = (check: string) => {
+  setTimeout(() => {
+    console.log(check);
+  }, 3000);
+};
+
+const game = async () => {
+  for (const word of words) {
+    langText.innerText = word.text;
+    await new Promise((resolve) => setTimeout(resolve, 3000));
+    showWord(word.translation);
+  }
+};
+
+game();
